5b904e1e19bfbd6ed62fd8ee1f3f6016e1ac211f
Zeeshan Ayaz <zeeshanayaz1@gmail.com>
2016-04-24 13:59:53 +0500

    added step 01 Datatype practice

---
+++ step01-DataType/src/DataType.java
@@ -0,0 +1,72 @@
+/**
+ * Created by Zeeshan on 4/24/2016.
+ */
+public class DataType {
+    public static void main(String[] args) {
+
+        //Variable Declaration & Initialization
+//        int a;
+//        a = 0;
+//        System.out.println(a);
+
+        //using Datatype : byte
+//        byte a = 100;
+//        byte b = 30;
+//        byte c = (byte)(a+b);
+//        System.out.println("Addition of a and b is byte by casting :"+c);
+
+        //using Datatype : int
+//        int length = 20;
+//        int breadth = 30;
+//        int area = length * breadth;
+//        System.out.println("Area or Quadrilateral = "+area+" sq meter");
+
+        //using Datatype : Char
+//        char a = 'A';
+//        char b = 'a';
+//        char c = (char)(a + b);
+//        System.out.println(c);
+
+//        char ch;
+//        ch = 'X';
+//        System.out.println("ch contains "+ch);
+//
+//        ch++;
+//        System.out.println("ch after increment contains "+ch);
+//
+//        ch = 90;
+//        System.out.println("ch after assigning 90 contains "+ch);
+//
+//        ch = 45+46;
+//        System.out.println("ch after performing addition contains "+ch);
+
+        //using Datatype : float
+//        float a = 2.6f;
+//        int b = (int)a;
+//        System.out.println(a);
+//        System.out.println(b);
+
+        //using Datatype : bool boolean
+//        boolean check;
+//
+//        check = true;
+//        System.out.println("check is "+check);
+//
+//        check = false;
+//        System.out.println("check is "+check);
+//
+//        if(check)
+//            System.out.println("This is not executed as check is false");
+//
+//        check = true;
+//        System.out.println("This is executed as check is True");
+
+
+        //using string
+//        String text = "Hello";
+//        System.out.println((char) (text[1]));
+
+
+
+    }
+}
