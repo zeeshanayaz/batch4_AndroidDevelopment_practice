e3299ddf9830cd31ff974de3761efe6fe20c3c6a
Zeeshan Ayaz <zeeshanayaz1@gmail.com>
2016-04-28 01:07:53 +0500

    Step 03 for taking user input on runtime

---
+++ step03-UserInput/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) throws java.io.IOException {
+	// write your code here
+        char a;
+
+        System.out.print("Enter any character : ");
+        a = (char) System.in.read();
+
+        System.out.println("You entered : "+a);
+    }
+}
