9b9541d54b48d7fc6a5fa652c2748668a9efb828
Zeeshan Ayaz <zeeshanayaz1@gmail.com>
2016-04-28 00:21:25 +0500

    Practice of Java Operators

---
+++ step02-Operators/src/com/company/Main.java
@@ -0,0 +1,69 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        int a = 10;
+//        int b = 5;
+//        int c;
+//        //Arithmetic Operation on integers
+//        c = a + b;              //Addition
+//        System.out.println("a + b = "+c);
+//        c = a - b;              //Subtraction
+//        System.out.println("a - b = "+c);
+//        c = a * b;              //Multiplication
+//        System.out.println("a * b = "+c);
+//        c = a / b;              //Division
+//        System.out.println("a / b = "+c);
+//        c = a % b;              //Modulus
+//        System.out.println("a % b = "+c);
+
+
+
+        //Arithmetic Operation on Characters
+//        char a = 'C';       //int 67
+//        char b = '0';       //int 48
+//        int c;
+//        c = a + b;          //Addition
+//        System.out.println((char) c);
+//        c = a - b;          //Subtraction
+//        System.out.println((char) c);
+//        c = a * b;          //Multiplication
+//        System.out.println((char) c);
+//        c = a / b;          //Division
+//        System.out.println((char) c);
+//        c = a % b;          //Modulus
+//        System.out.println((char) c);
+
+
+        //increment Decrement
+//        int x = 10;
+//        int y;
+//        System.out.println(x);
+//        y = x++;      //post increment, first assign then increment
+//        y = ++x;      //pre increment, first increment then assign
+//        System.out.println(x);
+//        System.out.println(y);
+
+        //Relational Operator
+//        int p, q, r;
+//        p = 10;q = 12;
+//
+//        if (p<q) System.out.println("P < Q");
+//        if (p>q) System.out.println("P > Q");
+//        if (p<=q) System.out.println("P <= Q");
+//        if (p>=q) System.out.println("P >= Q");
+//        if (p==q) System.out.println("P = Q");
+//        if (p!=q) System.out.println("P != Q");
+//        boolean a , b;
+//        a = true; b = false;
+//        if(a & b) System.out.println("a AND b  is true");
+//        if(a | b) System.out.println("a OR b  is true");
+//        if(!(a & b)) System.out.println("!(a AND b)  is true");
+//        if(a ^ b) System.out.println("a ^ b is true");
+
+
+
+    }
+}
