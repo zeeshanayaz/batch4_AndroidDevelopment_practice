1d44440e8cd8bd26e02aa0e844f09b2d98d71d4f
Zeeshan <zeeshanayaz1@gmail.com>
2016-05-29 04:52:51 -0700

    Lab Task 1 Pattern 28 May 2016

---
+++ lab_Task/28May2016/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i = 1; i<=4;i++)
+        {
+            for(int j = 1; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 3; i >=1;i--)
+        {
+            for (int j = i;j>=1;j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
