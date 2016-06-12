b0be2d4efcb44794d113642b77b413837a86bc2f
Zeeshan <zeeshan@gmani.com>
2016-06-12 05:00:45 -0700

    University Manegement System Lab Task 12 June 2016

---
+++ lab_Task/12June2016/UniversityManagementSystem/src/com/company/Student.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * Created by MT-2016 on 6/12/2016.
+ */
+public class Student
+{
+    public String name;
+    public int age;
+    public String gender;
+    public String course;
+
+
+    public Student(String name,int age,String gender,String course)
+    {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.course = course;
+    }
+
+}
---
+++ lab_Task/12June2016/UniversityManagementSystem/src/com/company/Teacher.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by MT-2016 on 6/12/2016.
+ */
+public class Teacher {
+    public String name;
+    public int age;
+    public String gender;
+    public String qualification;
+    
+}
