804d223278ee41ca54b53f3fc6520e4a148edaf7
Zeeshan Ayaz <zeeshanayaz1@gmail.com>
2016-08-03 00:54:43 +0500

    Coffee Order app completed Project 2 of Nanodegree

--- android/LabTask/CoffeeOrderApp/app/src/main/java/com/example/zeeshan/coffeeorderapp/MainActivity.java
+++ android/LabTask/CoffeeOrderApp/app/src/main/java/com/example/zeeshan/coffeeorderapp/MainActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    int prizeOfCoffee = 5;
+    int quantity = 0;
+    int payment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,29 +19,57 @@ public class MainActivity extends AppCompatActivity {
     }
 //Counter Increment Function
     public void counterIncrement(View view) {
-        TextView textView = (TextView) findViewById(R.id.cofeeCounter);
+        quantity++;
+        TextView textView = (TextView) findViewById(R.id.textView_cofeeCounter);
         String count = textView.getText().toString();
+
+        TextView textView_quantity = (TextView)findViewById(R.id.textView_cofeeCounter);
+        textView_quantity.setText(""+ quantity);
+        textView_quantity.setTextSize(50);
 
     }
 //Counter Decrement Function
     public void counterDecrement(View view) {
-        TextView textView = (TextView) findViewById(R.id.cofeeCounter);
-        String count = textView.getText().toString();
+        if (quantity>0)
+        {
+            quantity--;
+            TextView textView = (TextView) findViewById(R.id.textView_cofeeCounter);
+            String count = textView.getText().toString();
 
+            TextView textView_quantity = (TextView)findViewById(R.id.textView_cofeeCounter);
+            textView_quantity.setText(""+ quantity);
+            textView_quantity.setTextSize(50);
+        }
     }
 //Generate Billing Amount Function
-    public void generateBill(View view)
+    public void submitOrder(View view)
     {
-        TextView textView = (TextView) findViewById(R.id.cofeeCounter);
-        String count = textView.getText().toString();
+        if (quantity>0)
+        {
+            TextView textView = (TextView) findViewById(R.id.textView_cofeeCounter);
+            String count = textView.getText().toString();
 
-        TextView textView_amount = (TextView)findViewById(R.id.textView_bill);
-        textView_amount.setText("Your Billed amount is Rs. "+count);
-        textView_amount.setTextSize(30);
+            payment = quantity * prizeOfCoffee;
 
-        String Greeting = "Thanks for your Order";
+            TextView textView_amount = (TextView) findViewById(R.id.textView_bill);
+            textView_amount.setText("Your Billed amount is Rs. " + payment);
+            textView_amount.setTextSize(30);
 
-        Toast ss = Toast.makeText(this,Greeting, Toast.LENGTH_SHORT);
-        ss.show();
+            String Greeting = "Thanks for your Order";
+
+            Toast ss = Toast.makeText(this, Greeting, Toast.LENGTH_SHORT);
+            ss.show();
+        }
+        else
+        {
+            TextView textView_amount = (TextView) findViewById(R.id.textView_bill);
+            textView_amount.setText("We cannot Sell Purchase 0 coffee");
+            textView_amount.setTextSize(30);
+
+            String Greeting = "Please select some logical quantity of Coffee";
+            Toast ss = Toast.makeText(this, Greeting, Toast.LENGTH_SHORT);
+            ss.show();
+        }
     }
+
 }
